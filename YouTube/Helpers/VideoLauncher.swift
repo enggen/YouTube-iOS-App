4451bb36407d47cc18bd4ee069ca540fdef3ecda
Engen Sherpa <enggenapp@gmail.com>
2018-01-26 16:18:52 +0000

    Display Video Player Controls

---
+++ YouTube/Helpers/VideoLauncher.swift
@@ -0,0 +1,61 @@
+//
+//  VideoLauncher.swift
+//  YouTube
+//
+//  Created by Ang Sherpa on 25/01/2018.
+//  Copyright © 2018 PieCart Inc. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoPlayerView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .black
+        
+        let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
+        
+        if let url = URL(string: urlString) {
+            let player = AVPlayer(url: url)
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            self.layer.addSublayer(playerLayer)
+            playerLayer.frame = self.frame
+            player.play()
+        }
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+
+class VideoLauncher: NSObject {
+    func showVideoPlayer() {
+        if let keyWindow = UIApplication.shared.keyWindow {
+            let view = UIView(frame: keyWindow.frame)
+            view.backgroundColor = UIColor.white
+            
+            view.frame = CGRect(x: keyWindow.frame.width-10, y: keyWindow.frame.height-10, width: 30, height: 20)
+            
+            let height = keyWindow.frame.width * 9/16
+            let videoplayerFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
+            let videoPlayerView = VideoPlayerView(frame: videoplayerFrame)
+            view.addSubview(videoPlayerView)
+            
+            keyWindow.addSubview(view)
+            
+            UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
+                view.frame = keyWindow.frame
+            }, completion: { (completedAnimation) in
+                // Hide statueBar
+                UIApplication.shared.isStatusBarHidden = true
+            })
+        
+        }
+    }
+    
+    
+}
