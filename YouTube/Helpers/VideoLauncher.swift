a94932fd7b5766b899af3f78cc71774de807a64f
Engen Sherpa <enggenapp@gmail.com>
2018-01-28 16:28:43 +0000

    Display Video Player Controls

--- YouTube/Helpers/VideoLauncher.swift
+++ YouTube/Helpers/VideoLauncher.swift
@@ -10,19 +10,97 @@ import UIKit
 import AVFoundation
 
 class VideoPlayerView: UIView {
+    
+    let activityIndicatorView: UIActivityIndicatorView = {
+        let aiv = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+        aiv.translatesAutoresizingMaskIntoConstraints = false
+        aiv.startAnimating()
+        return aiv
+    }()
+    
+    var pausePlayButton: UIButton = {
+       let button = UIButton(type: .system)
+        let image = UIImage(named: "pause")
+        button.setImage(image, for: .normal)
+        button.isHidden = true
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = UIColor.white
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        
+        return button
+    }()
+    
+    var isPlaying = false
+    
+    @objc func handlePause() {
+        if isPlaying {
+            player?.pause()
+            let image = UIImage(named: "play")
+            pausePlayButton.setImage(image, for: .normal)
+            pausePlayButton.isHidden = false
+        } else {
+            player?.play()
+            let image = UIImage(named: "pause")
+            pausePlayButton.setImage(image, for: .normal)
+            pausePlayButton.isHidden = true
+            
+            
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
+    let controlsContainerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
+        return view
+    }()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
+        
+        setupPlayerView()
+        
+        controlsContainerView.frame = frame
+        addSubview(controlsContainerView)
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        
         backgroundColor = .black
         
+    }
+    
+    var player: AVPlayer?
+    
+    private func setupPlayerView() {
         let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
         
         if let url = URL(string: urlString) {
-            let player = AVPlayer(url: url)
+            player = AVPlayer(url: url)
             
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
-            player.play()
+            player?.play()
+            
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
+        }
+    }
+    
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = UIColor.clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
         }
     }
     
@@ -38,7 +116,7 @@ class VideoLauncher: NSObject {
             let view = UIView(frame: keyWindow.frame)
             view.backgroundColor = UIColor.white
             
-            view.frame = CGRect(x: keyWindow.frame.width-10, y: keyWindow.frame.height-10, width: 30, height: 20)
+            view.frame = CGRect(x: keyWindow.frame.width-10, y: keyWindow.frame.height-10, width: 10, height: 10)
             
             let height = keyWindow.frame.width * 9/16
             let videoplayerFrame = CGRect(x: 0, y: 0, width: keyWindow.frame.width, height: height)
